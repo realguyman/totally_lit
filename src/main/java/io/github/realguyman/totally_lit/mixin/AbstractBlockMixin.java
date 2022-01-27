3469cc56ec22ea62e0eac6f6279ed6a5550b9e94
realguyman <realguyman@protonmail.com>
2022-01-27 14:17:10 -0900

    Completely refactor mixins to use @Inject

---
+++ src/main/java/io/github/realguyman/totally_lit/mixin/AbstractBlockMixin.java
@@ -0,0 +1,58 @@
+package io.github.realguyman.totally_lit.mixin;
+
+import io.github.realguyman.totally_lit.Configuration;
+import io.github.realguyman.totally_lit.registry.BlockRegistry;
+import net.minecraft.block.*;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.tick.WorldTickScheduler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(AbstractBlock.class)
+public abstract class AbstractBlockMixin {
+    @Shadow @Deprecated public abstract void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random);
+
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
+    private void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (state.isOf(Blocks.TORCH) || state.isOf(Blocks.WALL_TORCH)) {
+            if (world.hasRain(pos) && random.nextFloat() < Configuration.INSTANCE.extinguishInRainChance) {
+                this.scheduledTick(state, world, pos, random);
+            } else if (Configuration.INSTANCE.extinguishOverTime) {
+                WorldTickScheduler<Block> scheduler = world.getBlockTickScheduler();
+                Block block = state.getBlock();
+
+                if (!scheduler.isQueued(pos, block) && !scheduler.isTicking(pos, block)) {
+                    world.createAndScheduleBlockTick(pos, block, Configuration.INSTANCE.burnDuration * 6_000);
+                }
+            }
+
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "scheduledTick", at = @At("HEAD"))
+    private void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (Configuration.INSTANCE.extinguishOverTime) {
+            boolean updated = false;
+
+            // TODO: Consider using tags instead of checking each individual block.
+            if (state.isOf(Blocks.TORCH)) {
+                updated = world.setBlockState(pos, BlockRegistry.UNLIT_TORCH.getDefaultState());
+            } else if (state.isOf(Blocks.WALL_TORCH)) {
+                updated = world.setBlockState(pos, BlockRegistry.UNLIT_WALL_TORCH.getDefaultState().with(WallTorchBlock.FACING, state.get(WallTorchBlock.FACING)));
+            }
+
+            if (updated) {
+                world.playSound(null, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.0625F, random.nextFloat() * 0.5F + 0.125F);
+            }
+        }
+    }
+}
---
+++ src/main/java/io/github/realguyman/totally_lit/mixin/BlockMixin.java
@@ -0,0 +1,48 @@
+package io.github.realguyman.totally_lit.mixin;
+
+import io.github.realguyman.totally_lit.Configuration;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.tick.QueryableTickScheduler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Block.class)
+public class BlockMixin {
+    @Inject(method = "hasRandomTicks", at = @At("HEAD"), cancellable = true)
+    private void hasRandomTicks(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        if ((state.isOf(Blocks.TORCH) || state.isOf(Blocks.WALL_TORCH)) && (Configuration.INSTANCE.extinguishOverTime || Configuration.INSTANCE.extinguishInRainChance > 0F)) {
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(method = "onPlaced", at = @At("HEAD"))
+    private void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack, CallbackInfo ci) {
+        if (!world.isClient() && Configuration.INSTANCE.extinguishOverTime && (state.isOf(Blocks.TORCH) || state.isOf(Blocks.WALL_TORCH))) {
+            QueryableTickScheduler<Block> scheduler = world.getBlockTickScheduler();
+            Block block = state.getBlock();
+
+            if (!scheduler.isQueued(pos, block) || !scheduler.isTicking(pos, block)) {
+                world.createAndScheduleBlockTick(pos, block, Configuration.INSTANCE.burnDuration * 6_000);
+            }
+        }
+    }
+
+    @Inject(method = "onBreak", at = @At("HEAD"))
+    private void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci) {
+        if (!world.isClient() && (state.isOf(Blocks.TORCH) || state.isOf(Blocks.WALL_TORCH))) {
+            ((ServerWorld) world).getBlockTickScheduler().clearNextTicks(new BlockBox(pos));
+        }
+    }
+}
