36c411cf5d72b25a1578e8555b30883cbcd67dcb
realguyman <realguyman@protonmail.com>
2022-03-04 11:19:16 -0900

    Add ability to ignite campfires with torches

--- src/main/java/io/github/realguyman/totally_lit/mixin/CampfireBlockMixin.java
+++ src/main/java/io/github/realguyman/totally_lit/mixin/CampfireBlockMixin.java
@@ -3,7 +3,15 @@ package io.github.realguyman.totally_lit.mixin;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.CampfireBlock;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.Items;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,5 +32,13 @@ public abstract class CampfireBlockMixin extends BlockWithEntity {
     @Inject(method = "getPlacementState", at = @At("RETURN"), cancellable = true)
     private void getPlacementState(ItemPlacementContext context, CallbackInfoReturnable<BlockState> cir) {
         cir.setReturnValue(cir.getReturnValue().with(CampfireBlock.LIT, false));
+    }
+
+    @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
+    private void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+        if ((player.getStackInHand(hand).isOf(Items.TORCH) || player.getStackInHand(hand).isOf(Items.SOUL_TORCH)) && CampfireBlock.canBeLit(state) && world.setBlockState(pos, state.with(CampfireBlock.LIT, true))) {
+            player.incrementStat(Stats.INTERACT_WITH_CAMPFIRE);
+            cir.setReturnValue(ActionResult.SUCCESS);
+        }
     }
 }
