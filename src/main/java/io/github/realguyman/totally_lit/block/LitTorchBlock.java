91fa2cb70fbe50243003fa7b63cc7f09dae18ef0
realguyman <realguyman@protonmail.com>
2022-03-19 14:06:54 -0800

    Add lit torch block apis

---
+++ src/main/java/io/github/realguyman/totally_lit/block/LitTorchBlock.java
@@ -0,0 +1,18 @@
+package io.github.realguyman.totally_lit.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.TorchBlock;
+import net.minecraft.particle.ParticleEffect;
+
+public class LitTorchBlock extends TorchBlock {
+    private final Block unlitBlock;
+
+    public LitTorchBlock(Settings settings, ParticleEffect particle, Block unlitBlock) {
+        super(settings, particle);
+        this.unlitBlock = unlitBlock;
+    }
+
+    public Block getUnlitBlock() {
+        return unlitBlock;
+    }
+}
---
+++ src/main/java/io/github/realguyman/totally_lit/block/LitWallTorchBlock.java
@@ -0,0 +1,18 @@
+package io.github.realguyman.totally_lit.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.WallTorchBlock;
+import net.minecraft.particle.ParticleEffect;
+
+public class LitWallTorchBlock extends WallTorchBlock {
+    private final Block unlitBlock;
+
+    public LitWallTorchBlock(Settings settings, ParticleEffect particleEffect, Block unlitBlock) {
+        super(settings, particleEffect);
+        this.unlitBlock = unlitBlock;
+    }
+
+    public Block getUnlitBlock() {
+        return unlitBlock;
+    }
+}
