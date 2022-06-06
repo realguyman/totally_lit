1770790c816a0eab9fc151cc9fd85254e47c7d59
realguyman <realguyman@protonmail.com>
2022-06-05 18:10:52 -0800

    Add glowstone torch

--- src/main/java/io/github/realguyman/totally_lit/registry/BlockRegistry.java
+++ src/main/java/io/github/realguyman/totally_lit/registry/BlockRegistry.java
@@ -1,9 +1,7 @@
 package io.github.realguyman.totally_lit.registry;
 
 import io.github.realguyman.totally_lit.TotallyLitModInitializer;
-import io.github.realguyman.totally_lit.block.UnlitLanternBlock;
-import io.github.realguyman.totally_lit.block.UnlitTorchBlock;
-import io.github.realguyman.totally_lit.block.UnlitWallTorchBlock;
+import io.github.realguyman.totally_lit.block.*;
 import net.minecraft.block.AbstractBlock.Settings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -11,6 +9,8 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlockRegistry {
+    public static final Block GLOWSTONE_TORCH = add("glowstone_torch", new NoParticleTorchBlock(Settings.copy(Blocks.TORCH)));
+    public static final Block GLOWSTONE_WALL_TORCH = add("glowstone_wall_torch", new NoParticleWallTorchBlock(Settings.copy(Blocks.TORCH)));
     public static final Block UNLIT_LANTERN = add("unlit_lantern", new UnlitLanternBlock(Settings.copy(Blocks.LANTERN).luminance(state -> 0), Blocks.LANTERN));
     public static final Block UNLIT_TORCH = add("unlit_torch", new UnlitTorchBlock(Settings.copy(Blocks.TORCH).luminance(state -> 0), null, Blocks.TORCH));
     public static final Block UNLIT_WALL_TORCH = add("unlit_wall_torch", new UnlitWallTorchBlock(Settings.copy(Blocks.WALL_TORCH).luminance(state -> 0), null, Blocks.WALL_TORCH));
--- src/main/java/io/github/realguyman/totally_lit/registry/ItemRegistry.java
+++ src/main/java/io/github/realguyman/totally_lit/registry/ItemRegistry.java
@@ -2,14 +2,12 @@ package io.github.realguyman.totally_lit.registry;
 
 import io.github.realguyman.totally_lit.TotallyLitModInitializer;
 import io.github.realguyman.totally_lit.item.UnlitTorchItem;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ItemRegistry {
+    public static final Item GLOWSTONE_TORCH = add("glowstone_torch", new WallStandingBlockItem(BlockRegistry.GLOWSTONE_TORCH, BlockRegistry.GLOWSTONE_WALL_TORCH, new Item.Settings().group(ItemGroup.DECORATIONS)));
     public static final Item UNLIT_LANTERN = add("unlit_lantern", new BlockItem(BlockRegistry.UNLIT_LANTERN, new Item.Settings().group(ItemGroup.DECORATIONS)));
     public static final Item UNLIT_TORCH = add("unlit_torch", new UnlitTorchItem(BlockRegistry.UNLIT_TORCH, BlockRegistry.UNLIT_WALL_TORCH, new Item.Settings().group(ItemGroup.DECORATIONS), Items.TORCH));
 
