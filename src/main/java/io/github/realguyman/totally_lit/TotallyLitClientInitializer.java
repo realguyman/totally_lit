bf77c53d9b1740487c58d06eaadc37329f7f46b5
realguyman <realguyman@protonmail.com>
2022-06-05 19:02:42 -0800

    Add glowstone lantern

--- src/main/java/io/github/realguyman/totally_lit/TotallyLitClientInitializer.java
+++ src/main/java/io/github/realguyman/totally_lit/TotallyLitClientInitializer.java
@@ -9,6 +9,6 @@ import static io.github.realguyman.totally_lit.registry.BlockRegistry.*;
 public class TotallyLitClientInitializer implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), GLOWSTONE_TORCH, GLOWSTONE_WALL_TORCH, UNLIT_LANTERN, UNLIT_TORCH, UNLIT_WALL_TORCH);
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), GLOWSTONE_LANTERN, GLOWSTONE_TORCH, GLOWSTONE_WALL_TORCH, UNLIT_LANTERN, UNLIT_TORCH, UNLIT_WALL_TORCH);
     }
 }
--- src/main/java/io/github/realguyman/totally_lit/registry/ItemRegistry.java
+++ src/main/java/io/github/realguyman/totally_lit/registry/ItemRegistry.java
@@ -8,6 +8,7 @@ import net.minecraft.util.registry.Registry;
 
 public class ItemRegistry {
     public static final Item GLOWSTONE_TORCH = add("glowstone_torch", new WallStandingBlockItem(BlockRegistry.GLOWSTONE_TORCH, BlockRegistry.GLOWSTONE_WALL_TORCH, new Item.Settings().group(ItemGroup.DECORATIONS)));
+    public static final Item GLOWSTONE_LANTERN = add("glowstone_lantern", new BlockItem(BlockRegistry.GLOWSTONE_LANTERN, new Item.Settings().group(ItemGroup.DECORATIONS)));
     public static final Item UNLIT_LANTERN = add("unlit_lantern", new BlockItem(BlockRegistry.UNLIT_LANTERN, new Item.Settings().group(ItemGroup.DECORATIONS)));
     public static final Item UNLIT_TORCH = add("unlit_torch", new UnlitTorchItem(BlockRegistry.UNLIT_TORCH, BlockRegistry.UNLIT_WALL_TORCH, new Item.Settings().group(ItemGroup.DECORATIONS), Items.TORCH));
 
