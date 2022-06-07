7038d2b2a1aa6851159eacf0ac73e0bb67e1e463
realguyman <realguyman@protonmail.com>
2022-06-07 00:25:54 -0800

    Add API for lantern items

---
+++ src/main/java/io/github/realguyman/totally_lit/item/UnlitLanternItem.java
@@ -0,0 +1,18 @@
+package io.github.realguyman.totally_lit.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+
+public class UnlitLanternItem extends BlockItem {
+    private final Item litItem;
+
+    public UnlitLanternItem(Block block, Settings settings, Item litItem) {
+        super(block, settings);
+        this.litItem = litItem;
+    }
+
+    public Item getLitItem() {
+        return litItem;
+    }
+}
