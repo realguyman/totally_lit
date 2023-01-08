d5883511693fc2e96c924d56a37aee806a6252f2
realguyman <realguyman@protonmail.com>
2023-01-08 01:08:10 -0900

    Remove unused import

--- src/main/java/io/github/realguyman/totally_lit/mixin/ItemMixin.java
+++ src/main/java/io/github/realguyman/totally_lit/mixin/ItemMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
