69ca616dbbadcfac2924160c1fca41bc10908bd3
realguyman <realguyman@protonmail.com>
2023-01-08 01:10:23 -0900

    Use regular JSON instead of JSON5

--- src/main/java/io/github/realguyman/totally_lit/TotallyLitModInitializer.java
+++ src/main/java/io/github/realguyman/totally_lit/TotallyLitModInitializer.java
@@ -4,13 +4,13 @@ import io.github.realguyman.totally_lit.configuration.Configuration;
 import io.github.realguyman.totally_lit.registry.BlockRegistry;
 import io.github.realguyman.totally_lit.registry.ItemRegistry;
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
 import net.fabricmc.api.ModInitializer;
 
 public class TotallyLitModInitializer implements ModInitializer {
     public static final String IDENTIFIER = "totally_lit";
-    private static final Configuration CONFIGURATION = AutoConfig.register(Configuration.class, PartitioningSerializer.wrap(JanksonConfigSerializer::new)).getConfig();
+    private static final Configuration CONFIGURATION = AutoConfig.register(Configuration.class, PartitioningSerializer.wrap(GsonConfigSerializer::new)).getConfig();
 
     @Override
     public void onInitialize() {
