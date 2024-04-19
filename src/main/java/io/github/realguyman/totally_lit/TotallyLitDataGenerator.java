72d1902d85324a55f4cff7bcb1b9ee12a0c19d02
realguyman <realguyman@protonmail.com>
2024-04-18 19:41:35 -0800

    Setup datagen

---
+++ src/main/java/io/github/realguyman/totally_lit/TotallyLitDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.realguyman.totally_lit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TotallyLitDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+    }
+}
