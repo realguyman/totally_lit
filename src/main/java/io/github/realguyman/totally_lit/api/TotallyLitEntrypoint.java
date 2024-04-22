3bc5dd68c74f4f33bd75641056b71e5b79a25581
realguyman <realguyman@protonmail.com>
2024-04-21 20:41:47 -0800

    Add javadoc

--- src/main/java/io/github/realguyman/totally_lit/api/TotallyLitEntrypoint.java
+++ src/main/java/io/github/realguyman/totally_lit/api/TotallyLitEntrypoint.java
@@ -9,14 +9,32 @@ public interface TotallyLitEntrypoint {
      */
     void buildMap();
 
+    /**
+     * Correlate a lit torch to an unlit torch.
+     *
+     * @param lit   The torch considered ignited.
+     * @param unlit The torch considered extinguished.
+     */
     default void addTorch(Block lit, Block unlit) {
         TotallyLit.TORCH_MAP.put(lit, unlit);
     }
 
+    /**
+     * Correlate a lit lantern to an unlit lantern.
+     *
+     * @param lit   The lantern considered ignited.
+     * @param unlit The lantern considered extinguished.
+     */
     default void addLantern(Block lit, Block unlit) {
         TotallyLit.LANTERN_MAP.put(lit, unlit);
     }
 
+    /**
+     * Correlate a lit jack o'lantern to an unlit jack o'lantern.
+     *
+     * @param lit   The jack o'lantern considered ignited.
+     * @param unlit The jack o'lantern considered extinguished.
+     */
     default void addJackOLantern(Block lit, Block unlit) {
         TotallyLit.JACK_O_LANTERN_MAP.put(lit, unlit);
     }
