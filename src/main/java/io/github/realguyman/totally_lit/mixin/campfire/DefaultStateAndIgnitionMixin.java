008e6034e2d42aeac98540895a194b21c0bcda3c
realguyman <realguyman@protonmail.com>
2024-04-22 18:09:31 -0800

    Refactor campfire mixins and require newer version of Fabric Loader because of new mixin features used

--- src/main/java/io/github/realguyman/totally_lit/mixin/campfire/DefaultStateAndIgnitionMixin.java
+++ src/main/java/io/github/realguyman/totally_lit/mixin/campfire/DefaultStateAndIgnitionMixin.java
@@ -1,12 +1,12 @@
 package io.github.realguyman.totally_lit.mixin.campfire;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import io.github.realguyman.totally_lit.TotallyLit;
 import io.github.realguyman.totally_lit.registry.TagRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.CampfireBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -29,13 +29,13 @@ public abstract class DefaultStateAndIgnitionMixin extends BlockWithEntity {
     }
 
     @Inject(method = "<init>", at = @At("RETURN"))
-    private void setDefaultLitState(boolean emitsParticles, int fireDamage, Settings settings, CallbackInfo ci) {
+    private void litStateWhenInitialized(boolean emitsParticles, int fireDamage, Settings settings, CallbackInfo ci) {
         setDefaultState(getDefaultState().with(CampfireBlock.LIT, TotallyLit.CONFIG.campfires.defaultLitStateWhenPlaced()));
     }
 
-    @Inject(method = "getPlacementState", at = @At("RETURN"), cancellable = true)
-    private void setDefaultLitStateWhenPlacing(ItemPlacementContext context, CallbackInfoReturnable<BlockState> cir) {
-        cir.setReturnValue(cir.getReturnValue().with(CampfireBlock.LIT, TotallyLit.CONFIG.campfires.defaultLitStateWhenPlaced()));
+    @ModifyReturnValue(method = "getPlacementState", at = @At("RETURN"))
+    private BlockState litStateWhenPlaced(BlockState original) {
+        return original.with(CampfireBlock.LIT, TotallyLit.CONFIG.campfires.defaultLitStateWhenPlaced());
     }
 
     @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
@@ -44,11 +44,7 @@ public abstract class DefaultStateAndIgnitionMixin extends BlockWithEntity {
         final boolean canBeIgnited = CampfireBlock.canBeLit(state);
 
         if (itemInHand.isIn(TagRegistry.CAMPFIRE_IGNITER_ITEMS) && canBeIgnited && world.setBlockState(pos, state.with(CampfireBlock.LIT, true))) {
-            if (itemInHand.isDamageable()) {
-                itemInHand.damage(1, player, playerInScope ->
-                    playerInScope.sendToolBreakStatus(hand)
-                );
-            }
+            itemInHand.damage(1, player, playerInScope -> playerInScope.sendToolBreakStatus(hand));
 
             world.playSound(null, pos, SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 0.125F, world.getRandom().nextFloat() * 0.5F + 0.125F);
             player.incrementStat(Stats.INTERACT_WITH_CAMPFIRE);
