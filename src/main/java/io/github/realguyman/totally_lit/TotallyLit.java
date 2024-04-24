dc8925a8d98aa3819506da0242238bfd8243b710
realguyman <realguyman@protonmail.com>
2024-04-23 22:50:30 -0800

    Remove completed TODO

--- src/main/java/io/github/realguyman/totally_lit/TotallyLit.java
+++ src/main/java/io/github/realguyman/totally_lit/TotallyLit.java
@@ -40,7 +40,6 @@ import java.util.Map;
 // TODO: Extinguish system: Add ability to extinguish light sources with water buckets in world
 
 // TODO: Implement automatic texture generation for unlit variations of blocks with this technique: https://fabricmc.net/wiki/tutorial:datagen_buckets
-// TODO: API: Move helpful classes, etc into API package, i.e., NoParticleTorchBlock
 // TODO: Integrate: Tips, Patchouli, and Lanterns Belong on Walls
 // TODO: Clean: Re-implement JSON files into Fabric's datagen to simplify project and prevent future errors
 // TODO: Test: Implement testmod and more gametests
