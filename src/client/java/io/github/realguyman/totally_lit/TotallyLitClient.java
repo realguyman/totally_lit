a55a6b053e59dd99459f0ef28d911d6fe0aed4b1
realguyman <realguyman@protonmail.com>
2024-11-19 16:55:14 -0500

    Add optional resourcepack to change campfire item textures to unlit

--- src/client/java/io/github/realguyman/totally_lit/TotallyLitClient.java
+++ src/client/java/io/github/realguyman/totally_lit/TotallyLitClient.java
@@ -2,13 +2,29 @@ package io.github.realguyman.totally_lit;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import static io.github.realguyman.totally_lit.registry.BlockRegistry.*;
 
 public class TotallyLitClient implements ClientModInitializer {
+    public static final String MOD_ID = "totally_lit";
+
     @Override
     public void onInitializeClient() {
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+            ResourceManagerHelper.registerBuiltinResourcePack(
+                    Identifier.of(MOD_ID, "unlit_campfire"),
+                    modContainer,
+                    Text.translatable("resourcePack.totally_lit.unlit_campfire.name"),
+                    ResourcePackActivationType.DEFAULT_ENABLED
+            );
+        });
+
         BlockRenderLayerMap.INSTANCE.putBlocks(
                 RenderLayer.getCutout(),
                 GLOWSTONE_LANTERN,
