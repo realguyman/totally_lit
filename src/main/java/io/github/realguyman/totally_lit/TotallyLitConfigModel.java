8e992d136019963d56ed50f3fa7437398d4afc54
realguyman <realguyman@protonmail.com>
2026-06-10 21:45:29 -0400

    Change default value of `replaceWithUnlitVariantsInContainers` to `true` because it should remain consistent with other defaults

--- src/main/java/io/github/realguyman/totally_lit/TotallyLitConfigModel.java
+++ src/main/java/io/github/realguyman/totally_lit/TotallyLitConfigModel.java
@@ -10,7 +10,7 @@ public class TotallyLitConfigModel {
     public boolean itemEntitiesExtinguishWhenSubmerged = true;
     public boolean itemsCanExtinguishInPlayerInventory = true;
     public boolean replaceWithUnlitVariantsInBundles = true;
-    public boolean replaceWithUnlitVariantsInContainers = false;
+    public boolean replaceWithUnlitVariantsInContainers = true;
     public boolean fireAspectIgnitesUnlitVariants = true;
     @RangeConstraint(min = 0, max = 64)
     public int caretakerCheckRadius = 16;
